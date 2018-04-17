e7c85dafbffca931b24a3df0778f1a266ce53b88
Rachele Morino <rachelekm@gmail.com>
2018-04-17 12:48:57 -0400

    POST endpoint /recipes

--- server.js
+++ server.js
@@ -47,6 +47,18 @@ app.post('/shopping-list', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
+app.post('/recipes', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'ingredients'];
+  for(let i =0; i < requiredFields.length; i++){
+    if(!(requiredFields[i] in req.body)){
+      const message = `Missing \`${requiredFields[i]}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);      
+    }
+  }
+  res.status(201).json(Recipes.create(req.body.name, req.body.ingredients));
+});
+
 
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
